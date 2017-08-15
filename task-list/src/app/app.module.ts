e5f4e12c5686e023ba1acf1598265832e054823a
Mike Rousos <Mike@MJRousos.com>
2017-08-15 18:00:45 -0400

    Extract TaskList into its own component to prepare for routing from app-root

--- task-list/src/app/app.module.ts
+++ task-list/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // NgModel
 
 import { AppComponent } from './components/app/app.component';
 import { TaskDetailComponent } from './components/task-detail/task-detail.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskService } from './services/task.service';
+import { MockTaskService } from './services/mock-task.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskDetailComponent
+    TaskDetailComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  // providers: [TaskService]
+  providers: [                                      // Services can be injected into a component (which will make them available
+    {provide:TaskService, useClass:MockTaskService} // to that component and its children), or into the module, (which will make them
+  ],                                                // availalbe to the entire module).
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- task-list/src/app/components/task-list/task-list.component.ts
+++ task-list/src/app/components/task-list/task-list.component.ts
@@ -1,23 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { TaskListComponent } from '../task-list/task-list.component';
+import { Task } from '../../models/task';
 import { TaskService } from '../../services/task.service';
 import { MockTaskService } from '../../services/mock-task.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: 'task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css'],
   // providers: [TaskService]
   providers: [{provide:TaskService, useClass:MockTaskService}]  // Services can be injected into a component (which will make them available
                                                                 // to that component and its children), or into the module, (which will make them
                                                                 // availalbe to the entire module).
 })
-export class AppComponent implements OnInit {
-  public title = 'Task Tracker';
+export class TaskListComponent implements OnInit {
+  public tasks: Task[];
+  public selectedTask: Task;
 
-  constructor() { }
+  constructor(private taskService: TaskService) {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.getTasks();
+  }
 
   alert = (message) => alert(message);
+
+  async getTasks(): Promise<void> {
+    this.tasks = await this.taskService.getTasks();
+  }
+
+  onSelect(task:Task): void {
+    this.selectedTask = task;
+  }
 }
